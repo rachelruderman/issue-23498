9f9303bba0997142f71a7f2944af1660ab32c449
Rachel <Raquel.rudermano@gmail.com>
2022-08-25 13:14:50 -0700

    add new page location

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,10 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('cypress/fixtures/example.html')
+    cy.visit('cypress/fixtures/page1.html')
+    cy.location('pathname').should('equal', '/cypress/fixtures/page1.html')
     cy.get('button').click()
-    cy.get('h1').should('exist')
+    cy.contains('h1', 'Page 2')
+    cy.location('pathname').should('equal', '/cypress/fixtures/page2.html')
   })
 })
