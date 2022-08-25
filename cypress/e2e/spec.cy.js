937ca78615735c7608039c1a135323a1c64864b0
Rachel <Raquel.rudermano@gmail.com>
2022-08-25 11:06:08 -0700

    Mirror issue more closely -- redirect to new page after button click

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,7 +2,8 @@
 describe('page', () => {
   it('works', () => {
     cy.visit('cypress/fixtures/example.html')
+    cy.url().should('equal', 'http://localhost:60961/cypress/fixtures/example.html')
     cy.get('button').click()
-    cy.get('h1').should('exist')
+    cy.contains('h1', 'Redirected to new page')
   })
 })
