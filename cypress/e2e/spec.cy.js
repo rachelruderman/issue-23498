444a6030c280b446a3a8b626a2a747c8dd107efb
Rachel <Raquel.rudermano@gmail.com>
2022-08-25 11:10:16 -0700

    Revert "Mirror issue more closely -- redirect to new page after button click"
    This reverts commit 937ca78615735c7608039c1a135323a1c64864b0.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,8 +2,7 @@
 describe('page', () => {
   it('works', () => {
     cy.visit('cypress/fixtures/example.html')
-    cy.url().should('equal', 'http://localhost:60961/cypress/fixtures/example.html')
     cy.get('button').click()
-    cy.contains('h1', 'Redirected to new page')
+    cy.get('h1').should('exist')
   })
 })
